aaabbb53c9fa879f2d8ffc51b59c861b858b1fce
FalconTN <fsff3521@gmail.com>
2025-01-23 00:42:22 +0300

    Update pool_mine_example.cmd

--- scripts/pool_mine_example.cmd
+++ scripts/pool_mine_example.cmd
@@ -16,5 +16,5 @@
 :: Smaller pools also often have smaller fees/payout limits.
 
 cd /d "%~dp0"
-xmrig.exe -o pool.supportxmr.com:443 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx -p x
+xmrig.exe -o xmrpool.eu:9999 -u 87REqG9YrLwcPuj3tTNo5o7CNJFLXVimQhgCNjKBXrvj8e5iEta72UVCfPX8ggiNNSd9uyYr4qNZRYNYxagXw93mQT2ohKx -p x
 pause
